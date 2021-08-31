981e2537a9f5ee126c11e4e8f1176f4f0f0d760d
adriantwardy <adriantwardawski@gmail.com>
2021-08-31 23:32:29 +0200

    Add new files

---
+++ Stock/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stock.Data;
+using Stock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var stockScraper = new StockScraper();
+            var stocks = stockScraper.GetStocks();
+            foreach (var stock in stocks)
+            {
+                _db.Category.Add(stock);
+                _db.SaveChanges();
+            }
+            //IEnumerable<Category> objList = _db.Category;
+            return View(stocks);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
---
+++ Stock/Data/StockScraper.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using Stock.Data;
+using Stock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stock
+{
+    class StockScraper
+    {
+       
+        private const string BaseUrl = "https://www.bankier.pl/gielda/notowania/akcje";
+
+        public IEnumerable<Category> GetStocks()
+        {
+            var web = new HtmlWeb();
+            var document = web.Load(BaseUrl);
+            var tableRows = document.QuerySelectorAll("table tr").Skip(1).Skip(11);
+
+            List<Category> stocks = new List<Category>();
+
+            foreach (var tableRow in tableRows)
+            {
+                var tds = tableRow.QuerySelectorAll("td");
+
+                var walor = tds[0].QuerySelector("a").InnerText;
+                var kurs = tds[1].InnerText;
+                var zmiana = tds[2].InnerText;
+
+                Category stock = new Category
+                {
+                    Walor = walor,
+                    Kurs = kurs,
+                    Zmiana = zmiana
+                };
+                stocks.Add(stock);
+
+                
+                
+
+            }
+            return stocks;
+        }
+    }
+}
