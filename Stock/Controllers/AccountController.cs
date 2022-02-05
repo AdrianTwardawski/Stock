c6cd1ffc8f966b1f2fd5cd415bb7e2acb9230dfe
adriantwardy <adriantwardawski@gmail.com>
2022-02-05 13:20:43 +0100

    Register Implementation

--- Stock/Controllers/AccountController.cs
+++ Stock/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Stock.Data;
+using Stock.Models;
 using Stock.Models.ViewModels;
+using Stock.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +14,17 @@ namespace Stock.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ApplicationDbContext dbContext)
+        public AccountController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
     
@@ -24,8 +34,37 @@ namespace Stock.Controllers
         }
 
      
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
+            if(!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.NormalUser));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.PremiumUser));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if(ModelState.IsValid) /*Server side validation*/
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return View();
         }
     }
